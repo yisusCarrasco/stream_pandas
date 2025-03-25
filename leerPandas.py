81bcbb37c7df1c951f528ca0babc8b9b616b07ce
Enrique <eacb2012p@gmail.com>
2025-03-25 09:08:31 -1100

    practica1

--- leerPandas.py
+++ leerPandas.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("dataset/traj_UNI_CORR_500_01.txt", 
+
+variable = "dataset/traj_UNI_CORR_500_01.txt"
+df = pd.read_csv(variable, 
                  skiprows=4, 
                  sep="\t", 
                  names=["ID", "frames", "X", "Y", "Z"])
